45dee032126851636fda6e99f5917584654b816b
linet4 <linetwahome2017@gmail.com>
2024-04-11 20:50:26 +0300

    update

---
+++ binding.js
@@ -0,0 +1,12 @@
+// getting js data to html
+
+// let f_name = "linet"
+// let s_name = "wahome"
+// let full_name = f_name + " " + s_name
+// document.getElementById("first").innerHTML = full_name
+
+// getting html data to js
+
+let form=document.getElementById("myform")
+
+form.addEventListener("submit")
